0f89dec8b0634c025b1372310dee3a7973ea6cb6
mannast <anna-stephany@gmx.de>
2022-05-16 19:45:15 +0200

    test callback

--- linda/test/TestsLinda2.java
+++ linda/test/TestsLinda2.java
@@ -1,5 +1,6 @@
 package linda.test;
 
+import linda.Linda;
 import linda.Tuple;
 import linda.shm.CentralizedLinda;
 
@@ -8,4 +9,90 @@ import java.util.Collection;
 // test des callbacks
 
 public class TestsLinda2 {
+
+        // partie 2 : Test des callbacks
+    public static void main(String[] args) {
+
+        Tuple tupleA = new Tuple(1, "1");
+        Tuple tupleB = new Tuple(2, "1");
+        Tuple tupleC = new Tuple(1, "2",10);
+        Tuple motifA = new Tuple(1, String.class);
+        Tuple motifB = new Tuple(2, String.class);
+        Tuple motifAB = new Tuple(Integer.class, "1");
+        Tuple motifC = new Tuple(1, String.class, 10);
+        Tuple motifABC = new Tuple(Integer.class, String.class);
+
+        var test1 = new Test(){
+            Linda linda = new CentralizedLinda();
+
+            public void test() {
+                orderedRun(()-> {
+
+                    linda.write(tupleA);
+                    linda.debug("write A");
+
+                } );
+
+                orderedRun(()-> {
+
+                    linda.write(tupleB);
+                    linda.debug("write B");
+
+                } );
+
+                orderedRun(()-> {
+
+                    linda.write(tupleC);
+                    linda.debug("write C");
+
+                } );
+
+                orderedRun(()-> {
+
+                    linda.eventRegister(Linda.eventMode.READ, Linda.eventTiming.IMMEDIATE,motifA,t -> {
+                        System.out.println("callback Read Immediate motifA " + t);
+                        linda.debug("apres callback read Immediate motif A");
+                    });
+
+                });
+
+                orderedRun(()-> {
+
+                    linda.eventRegister(Linda.eventMode.READ, Linda.eventTiming.FUTURE,motifB,t -> {
+                        System.out.println("callback Read immediate motifB " + t);
+                        linda.debug("apres callback read future motif B");
+                    });
+
+                });
+
+                orderedRun(()-> {
+
+                    linda.eventRegister(Linda.eventMode.TAKE, Linda.eventTiming.FUTURE,motifC,t -> {
+                        System.out.println("callback Take immediate motif C " + t);
+                        linda.debug("apres callback take future motif C");
+                    });
+
+                });
+
+                orderedRun(()-> {
+
+                    linda.eventRegister(Linda.eventMode.TAKE, Linda.eventTiming.IMMEDIATE,motifB,t -> {
+                        System.out.println("callback Take immediate motifB " + t);
+                        linda.debug("apres callback take future motif B");
+                    });
+
+                });
+
+                orderedRun(()-> {
+
+                    linda.write(tupleC);
+                    linda.debug("write C");
+
+                } );
+
+            }
+
+        };
+        test1.run();
+    }
 }
